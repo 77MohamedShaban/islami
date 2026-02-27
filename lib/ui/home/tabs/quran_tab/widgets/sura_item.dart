55a22078c87f9af03fb4ee6c9971ea076ea8465e
Mohamed Shaban <mohamed.shabanzakaryia@gmail.com>
2026-02-28 01:01:07 +0200

    + end feature quran

--- lib/ui/home/tabs/quran_tab/widgets/sura_item.dart
+++ lib/ui/home/tabs/quran_tab/widgets/sura_item.dart
@@ -3,6 +3,7 @@ import 'package:flutter_svg/flutter_svg.dart';
 import 'package:islami/core/resources/assets_manager.dart';
 import 'package:islami/core/resources/colors_manager.dart';
 import 'package:islami/model/sura_model.dart';
+import 'package:islami/ui/sura_details/screen/sura_details_screen.dart';
 
 class SuraItem extends StatelessWidget {
  final SuraModel sura;
@@ -10,24 +11,29 @@ class SuraItem extends StatelessWidget {
 
   @override
   Widget build(BuildContext context) {
-    return Row(children: [
-      Stack(
-        alignment: Alignment.center,
-        children: [
-          SvgPicture.asset(AssetsManager.quranFrame),
-          Text(sura.suraNumber.toString(),style: TextStyle(fontSize: 20,fontWeight: FontWeight.w700,color: ColorsManager.white),)
-        ],
-      ),
-      SizedBox(width: 24,),
-      Expanded(
-        child: Column(
-        crossAxisAlignment: CrossAxisAlignment.start
-        ,children: [
-          Text(sura.suraNameEn,style: TextStyle(fontSize:20 ,fontWeight: FontWeight.w700,color: ColorsManager.white),)
-          ,Text("${sura.versesNumber} Verses",style: TextStyle(fontSize:14 ,fontWeight: FontWeight.w700,color: ColorsManager.white),)
-        ],),
-      ),
-      Text(sura.suraNameAr,style: TextStyle(fontSize:20 ,fontWeight: FontWeight.w700,color: ColorsManager.white),)
-    ],);
+    return InkWell(
+      onTap: () {
+        Navigator.pushNamed(context,SuraDetailsScreen.routeName,arguments: sura);
+      }
+      ,child: Row(children: [
+        Stack(
+          alignment: Alignment.center,
+          children: [
+            SvgPicture.asset(AssetsManager.quranFrame),
+            Text(sura.suraNumber.toString(),style: TextStyle(fontSize: 20,fontWeight: FontWeight.w700,color: ColorsManager.white),)
+          ],
+        ),
+        SizedBox(width: 24,),
+        Expanded(
+          child: Column(
+          crossAxisAlignment: CrossAxisAlignment.start
+          ,children: [
+            Text(sura.suraNameEn,style: TextStyle(fontSize:20 ,fontWeight: FontWeight.w700,color: ColorsManager.white),)
+            ,Text("${sura.versesNumber} Verses",style: TextStyle(fontSize:14 ,fontWeight: FontWeight.w700,color: ColorsManager.white),)
+          ],),
+        ),
+        Text(sura.suraNameAr,style: TextStyle(fontSize:20 ,fontWeight: FontWeight.w700,color: ColorsManager.white),)
+      ],),
+    );
   }
 }
